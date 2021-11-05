ac964f61214686dd30d8fae56e2e50f7d858dedc
DevXerxes <87164130+DevXerxes@users.noreply.github.com>
2021-11-04 18:40:35 -0700

    File Transfer program resubmission
    resubmission for final draft. launch day

--- FIO3Tk.py
+++ FIO3Tk.py
@@ -10,131 +10,91 @@
 
 from tkinter.filedialog import askdirectory
 import datetime
+from datetime import *
 import glob
 import os
 import shutil
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+from datetime import timedelta
+from datetime import datetime
 
 
 # This is the initiation for the main window for tkinter GUI
-class mainWindow(Frame):
-    def __init__(self, parent, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.parent.title("File Transfer Process")
-        self.parent.geometry("")
-        arg = self.parent
-        self.initialize()
-
-    # the function to create the layout for the GUI
-    def initialize(self):
-        
-
-        self.grid()
-
-        self.copyTextLabel = tk.Label(self, anchor = "e", text="Copy text files modified since:")
-        self.copyTextLabel.grid(column=0, row=1)
-
-
-        # Display the last date and time files were transferred
-        self.lastTransferDate = tk.Label(self, anchor="w", text = "24 hours", width=33)
-        self.lastTransferDate.grid(column=1, row=1)
-        
-
-
-        # Source folder elements
-        self.sourceFolderLabel = tk.Label(self, anchor="e", text="Source Folder:", width=22)
-        self.sourceFolderLabel.grid(column=0, row=2)
-        self.sourceFolderText = tk.Text(self, state="disabled", width=30, height=1)
-        self.sourceFolderText.grid(column=1, row=2)
-        self.sourceBrowseButton = tk.Button(self, text="Browse", command=self.sourceBrowse)
-        self.sourceBrowseButton.grid(column=2, row=2, padx=5)
-
-
-        # Destination Folder Elements
-        self.destFolderLabel = tk.Label(self, anchor="e", text="Destination Folder:", width=22)
-        self.destFolderLabel.grid(column=0, row=3)
-        self.destFolderText = tk.Text(self, state="disabled", width=30, height=1)
-        self.destFolderText.grid(column=1, row=3)
-        self.destBrowseButton = tk.Button(self, text="Browse", command=self.destBrowse)
-        self.destBrowseButton.grid(column=2, row=3, columnspan=3, pady=5)
-
-        # Copy button element
-        self.copyFilesButton = tk.Button(self, text="Copy Files", command=self.copyFiles)
-        self.copyFilesButton.grid(column=0, row=4, columnspan=3, pady=5)
-
-
-    def sourceBrowse(self):
-        '''
-        Select an Source Folder to look for the text files
-        '''
-
-        # The event to help select the source folder
-        dlg = tk.filedialog.askdirectory(parent=self, initialdir="/", title="Choose the Source Folder:")
-        print(dlg)
-        if len(dlg) > 0:
-            self.destFolderText.config(state="normal")
-            self.destFolderText.delete('1.0', 'end')
-            self.destFolderText.insert('1.0', dlg)
-            self.destFolderText.config(state="disabled")
-
-
-    def destBrowse(self):
-        '''
-        Select a Destinaton Folder to copy the files to
-        '''
-        
-        dlg2 = tk.filedialog.askdirectory(parent=self, initialdir="/", title="Choose Destinaton Folder:")
-        print(dlg2)
-        if len(dlg2) > 0:
-            self.destFolderText.config(state="normal")
-            self.destFolderText.delete('1.0', 'end')
-            self.destFolderText.insert('1.0', dlg2)
-            self.destFolderText.config(state="disabled")
-
-
-    def copyFiles(self):
-        # Perform the copy function on files
-
-        # Find the source and destination paths (new lines will need to be replaced)
-        sourcePath = self.sourceFolderText.get('1.0', 'end').replace('\n','') + "\\"
-        destPath = self.destFolderText.get('1.0', 'end').replace('\n','') + "\\"
-
-        fileType = ".txt"
-
-
-        # Create list of text filenames in source folder
-        fileList = glob.glob(sourcePath + "*" + fileType)
 
 
 
-        for file in fileList:
-            # get last modified date and today's date
-            modifyDate = datetime.datetime.fromtimestamp(os.path.getmtime(file))
+def sourceBrowse():
+    '''
+    Select an Source Folder to look for the text files
+    '''
+    # The event to help select the source folder
+    src = tk.filedialog.askdirectory()
+    sourceFolderText.insert(0, src)
 
-            filePathList = file.split("\\") # Create a list from the filepath
-            filename = filePathList[-1] # the last element is the filename
+def destBrowse():
+    '''
+    Select a Destinaton Folder to copy the files to
+    '''
+    dest = tk.filedialog.askdirectory()
+    destFolderText.insert(0, dest)
 
 
-            if modifyDate > dtLastDateTime:
-                shutil.copy2(file, destPath + filename)
-
-
-        # Insert new row into the database
-        todaysDate = datetime.datetime.today().strftime("%m-%d-%Y %H:%M:%S")
+def copyFiles():
+    src = sourceFolderText.get()
+    des = destFolderText.get()
+    sourceFiles = os.listdir(src)
+    for i in sourceFiles:
+        filePath = os.path.join(src, i)
+        timepaste = os.path.getmtime(filePath)
+        dateFiletime = datetime.fromtimestamp(timepaste)
+        if dateFiletime > (datetime.now()-timedelta(hours = 24)):
+            shutil.move(src + "/" + i, des)
+            print(i + "Transferred")
+       
         
 
 
-        # Update GUI with new date and time
-        self.lastTransferDate.config(text=todaysDate)
+      
+
+m = tk.Tk()
+
+# create the layout for the GUI
+    
+        
+copyTextLabel = tk.Label(m, anchor = "e", text="Copy text files modified since:")
+copyTextLabel.grid(column=0, row=1)
 
 
-if __name__=="__main__":
-    root = tk.Tk()
-    app = mainWindow(root)
-    root.mainloop()
+# Display the last date and time files were transferred
+lastTransferDate = tk.Label(m, anchor="w", text = "24 hours", width=33)
+lastTransferDate.grid(column=1, row=1)
+
+
+
+# Source folder elements
+sourceFolderLabel = tk.Label(m, anchor="e", text="Source Folder:", width=22)
+sourceFolderLabel.grid(column=0, row=2)
+sourceFolderText = tk.Entry(m, width=100)
+sourceFolderText.grid(column=1, row=2)
+sourceBrowseButton = tk.Button(m, text="Browse", command=sourceBrowse)
+sourceBrowseButton.grid(column=2, row=2, padx=5)
+
+
+# Destination Folder Elements
+destFolderLabel = tk.Label(anchor="e", text="Destination Folder:", width=22)
+destFolderLabel.grid(column=0, row=3)
+destFolderText = tk.Entry(m, width=100)
+destFolderText.grid(column=1, row=3)
+destBrowseButton = tk.Button(text="Browse", command=destBrowse)
+destBrowseButton.grid(column=2, row=3, columnspan=3, pady=5)
+
+# Copy button element
+copyFilesButton = tk.Button(text="Copy Files", command=copyFiles)
+copyFilesButton.grid(column=0, row=4, columnspan=3, pady=5)
+
+m.mainloop()
 
 
         
